3e678a89671063e48673fd3e507c9ad7c475cd76
Alon Erik <97376715+AlonErik@users.noreply.github.com>
2022-03-29 13:34:41 +0800

    Add files via upload

---
+++ C++Primer/Chapter04/task.cpp
@@ -0,0 +1,25 @@
+/*
+4.28
+输出每一种内置类型所占空间的大小
+{
+	cout << "类型名称\t\t" << "所占空间\t" << endl;
+	cout << "bool\t\t\t" << sizeof(bool) << endl;
+	cout << "char\t\t\t" << sizeof(char) << endl;
+	cout << "wchar_t\t\t\t" << sizeof(wchar_t) << endl;
+	cout << "char16_t\t\t" << sizeof(char16_t) << endl;
+	cout << "char32_t\t\t" << sizeof(char32_t) << endl;
+	cout << "short\t\t\t" << sizeof(short) << endl;
+	cout << "int\t\t\t" << sizeof(int) << endl;
+	cout << "long\t\t\t" << sizeof(long) << endl;
+	cout << "long long\t\t" << sizeof(long long) << endl;
+	cout << "float\t\t\t" << sizeof(float) << endl;
+	cout << "double\t\t\t" << sizeof(double) << endl;
+	cout << "long double\t\t" << sizeof(long double) << endl;
+	cout << "unsigned\t\t" << sizeof(unsigned) << endl;
+	cout << "unsigned char\t\t" << sizeof(unsigned char) << endl;
+	cout << "unsigned short\t\t" << sizeof(unsigned short) << endl;
+	cout << "unsigned int\t\t" << sizeof(unsigned int) << endl;
+	cout << "unsigned long\t\t" << sizeof(unsigned long) << endl;
+	cout << "unsigned long long\t" << sizeof(unsigned long long) << endl;
+}
+*/
